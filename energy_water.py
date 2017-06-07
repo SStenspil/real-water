17c7bb7c88b2282a10f1e006f3b15966c9db0dc3
sygojesper <jesper3@gmail.com>
2017-06-07 16:03:39 +0200

    Add files via upload

--- energy_water.py
+++ energy_water.py
@@ -2,77 +2,63 @@ import pybel as pb
 import molecule as m
 import openbabel as ob
 import numpy as np
+import matplotlib.pyplot as plt
+
 
 #read file 
 mol = pb.readfile("xyz","w6.xyz").next()
 w = open('w6.xyz')
+lines_after_2 = w.readlines()[2:] #removes to first lines
 # defines forcefield MMFF94
 forcefield = ob.OBForceField.FindForceField("MMFF94")
-
-# Number of dihedral angles corresponding to water molecules
-no_dihedral = 1
-
 
 #calculate MMFF94 force field energy
 energy = m.get_energy(mol)
 
+no_atoms = 4
 
-for line in w:
+atom = []
+x = []
+y = []
+z = []
+
+for line in lines_after_2:
     line = line.split()
-    
+    atom.append(line[0])
+    x.append(float(line[1]))
+    y.append(float(line[2]))
+    z.append(float(line[3]))
 
-
-mols =
-print mols
 
 """
-# dihedral angles
-angles = np.random.uniform(0.0, 120.0, no_dihedral)
-m.set_dihedral(angles, mol, forcefield)
-
-m.save_molecule(water2.xyz)
+Run it here
 """
 
+print m.get_energy(mol)
 
+mol = m.set_dihedral(mol, forcefield,x,y,z)
 
-# rotate H1 about O-H2 bond
-
-def R(theta, u):
-    return [[np.cos(theta) + u[0]**2 * (1-np.cos(theta)), 
-             u[0] * u[1] * (1-np.cos(theta)) - u[2] * np.sin(theta), 
-             u[0] * u[2] * (1 - np.cos(theta)) + u[1] * np.sin(theta)],
-            [u[0] * u[1] * (1-np.cos(theta)) + u[2] * np.sin(theta),
-             np.cos(theta) + u[1]**2 * (1-np.cos(theta)),
-             u[1] * u[2] * (1 - np.cos(theta)) - u[0] * np.sin(theta)],
-            [u[0] * u[2] * (1-np.cos(theta)) - u[1] * np.sin(theta),
-             u[1] * u[2] * (1-np.cos(theta)) + u[0] * np.sin(theta),
-             np.cos(theta) + u[2]**2 * (1-np.cos(theta))]]
-
-def Rotate(pointToRotate, point1, point2, theta):
-
-
-    u= []
-    squaredSum = 0
-    for i,f in zip(point1, point2):
-        u.append(f-i)
-        squaredSum += (f-i) **2
-
-    u = [i/squaredSum for i in u]
-
-    r = R(theta, u)
-    rotated = []
-
-    for i in range(3):
-        rotated.append(round(sum([r[j][i] * pointToRotate[j] for j in range(3)])))
-
-    return rotated
-
-
-point = []
-p1 = []
-p2 = []
-
-#print Rotate(point, p1, p2, np.pi)
+print m.get_energy(mol)
 
 
 
+"""
+#Its important to call this function before the others as it generates the global variable mol
+mol.generate_chain(no_atoms)
+# Number of dihedral angles corresponding
+# to the molecule alkane chain
+
+
+# Create a list of random dihedral angles
+# between 0.0 and 360.0 degrees
+dihedral_list = np.random.uniform(0.0, 360.0, no_dihedral)
+
+mol.set_dihedral(dihedral_list)
+
+energy = mol.get_energy()
+
+
+mol.save_molecule('butane.xyz')
+
+print "The energy for \omega = ", dihedral_list, "is", energy
+"""
--- molecule.py
+++ molecule.py
@@ -2,12 +2,12 @@ import openbabel as ob
 import pybel as pb
 import numpy as np
 
-global mol
-global forcefield
+#global mol
+#global forcefield
 
 
 def get_energy(mol):
-    """ Returns the MMFF94 energy of the molecule in kcal/mol.
+    """ Returns the GAFF energy of the molecule in kcal/mol.
     """
 
 #    global mol
@@ -17,10 +17,56 @@ def get_energy(mol):
 
     return FF.Energy()
 
+def choose_atoms():
+    #for i in range(10):
+        random = np.random.randint(2)
+        O1 = np.random.randint(6)*3 #random O
+        if random == 1:
+            H1 = O1+1
+            H2 = O1+2
+        else:
+            H1 = O1+2
+            H2 = O1+1
+        O2 = np.random.randint(6)*3
+        if O1 == O2:
+            O2 = np.random.randint(6)*3
+        
+        return O1, H1, H2, O2
 
-def set_dihedral(angles, mol, forcefield):
-    """ Set the dihedral angles of the carbon chain
-    """
+#calculate dihedral angle https://math.stackexchange.com/questions/47059/how-do-i-calculate-a-dihedral-angle-given-cartesian-coordinates
+def find_angle(n_O1, n_H1, n_H2, n_O2, x, y, z):
+
+    O1 = np.array([x[n_O1], y[n_O1], z[n_O1]])
+    H1 = np.array([x[n_H1], y[n_H1], z[n_H1]])
+    H2 = np.array([x[n_H2], y[n_H2], z[n_H2]])
+    O2 = np.array([x[n_O2], y[n_O2], z[n_O2]])
+
+    #vector coordinates by vector substraction
+    b1 = O1 - H1
+    b2 = H2 - O1
+    b3 = O2 - H2
+
+    # normalvector
+    c1 = np.cross(b1,b2)
+    c2 = np.cross(b2,b3)
+
+    n1 = c1/np.linalg.norm(c1)
+    n2 = c2/np.linalg.norm(c2)
+
+    b2_norm = b2/np.linalg.norm(b2)
+
+    m1 = np.cross(n1,b2_norm)
+
+    #coordinates on orthonormal frame by n1, b2_norm and m1
+    x = np.dot(n1,n2)
+    y = np.dot(m1,n2)
+
+    #dihedral angle
+    angle = np.arctan2(x,y)
+    
+    return angle
+
+def set_dihedral(mol, forcefield,x,y,z):
 
 #    global mol
 #    global forcefield
@@ -29,38 +75,27 @@ def set_dihedral(angles, mol, forcefield):
     # constraints.AddDistanceConstraint(1, 10, 3.4)       # Angstroms
     # constraints.AddAngleConstraint(1, 2, 3, 120.0)      # Degrees
     # constraints.AddTorsionConstraint(1, 2, 3, 4, 180.0) # Degrees
+    
+    for i in range(2):
+        n_O1, n_H1, n_H2, n_O2 = choose_atoms()
+        angle = find_angle(n_O1, n_H1, n_H2, n_O2 ,x,y,z)
 
+        a = mol.OBMol.GetAtom(n_H1)
+        b = mol.OBMol.GetAtom(n_O1)
+        c = mol.OBMol.GetAtom(n_H2)
+        d = mol.OBMol.GetAtom(n_O2)
 
-    # Find all oxygens
-    smarts = pb.Smarts("O")
-    smarts = smarts.findall(mol)
-
-    for i in xrange(len(angles)):
-
-        angle = angles[i]
-
-        # Get the next 4 carbons
-        Cs = smarts[i:i+4]
-
-        ai, = Cs[0]
-        bi, = Cs[1]
-        ci, = Cs[2]
-        di, = Cs[3]
-
-        a = mol.OBMol.GetAtom(ai)
-        b = mol.OBMol.GetAtom(bi)
-        c = mol.OBMol.GetAtom(ci)
-        d = mol.OBMol.GetAtom(di)
-
-
-        mol.OBMol.SetTorsion(a, b, c, d, angle/(180.0)*np.pi) # Radians
+        mol.OBMol.SetTorsion(a, b, c, d, angle)
         anglep = mol.OBMol.GetTorsion(a, b, c, d)
+        
 
-        # Define constraint
-        constraints.AddTorsionConstraint(ai, bi, ci, di, anglep) # Degrees
+        # Define constraint fjernet for "ai" ikke giver mening
+        # constraints.AddTorsionConstraint(a, b, c, d, anglep) # Degrees
+        
+        print "rotate"
 
     # Setup the force field with the constraints
-#    forcefield = ob.OBForceField.FindForceField("MMFF94")
+    # forcefield = ob.OBForceField.FindForceField("MMFF94")
     forcefield.Setup(mol.OBMol)
     forcefield.SetConstraints(constraints)
     forcefield.EnableCutOff(True) # VDW cutoff
@@ -72,8 +107,7 @@ def set_dihedral(angles, mol, forcefield):
 
 
 def find_local_min(mol, forcefield):
-    """
-    """
+
 
 #    global mol
 #    global forcefield
@@ -91,7 +125,7 @@ def save_molecule(filename):
 
  #   global mol
 
-    mol.write('xyz', filename, overwrite="True")
+    mol.write('w6.xyz', filename, overwrite="True")
 
 
 if __name__ == '__main__':
@@ -113,4 +147,3 @@ if __name__ == '__main__':
     print get_energy()
 
     # save_molecule('carbon_man3.xyz')
-
