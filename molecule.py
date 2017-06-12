6930da93826bb0d06d6f40cc8d0f937ed92aec38
sygojesper <jesper3@gmail.com>
2017-06-12 16:12:58 +0200

    Add files via upload

--- molecule.py
+++ molecule.py
@@ -81,6 +81,7 @@ def perturb(x,y,z, atom, no_atom):
             
     return x, y, z
 
+
 def random_konf():
 #for i in range(1):
     #read file 
@@ -139,16 +140,29 @@ def random_konf():
         energy_new = get_energy(mol_new)
 
               
-        print "Starting energy:", energy_new
+        
+        
+        return energy_new
 
 
-def hastings_all(n_gen,T,R,c):
-    energy_list = [0 for n_gen in range(n_gen)] #new aproach
+def hastings_all(n_gen,T,R,c,energy_list):
     percent = -10 #percentage counter
+    
+    os.system("cp water_HE.xyz water_HE_temp.xyz")
+    
     for j in range(n_gen):
         #read file 
-        mol_old = pb.readfile("xyz","water_HE.xyz").next()
-        w = open('water_HE.xyz')
+        
+        if j == 0:
+            w = open('water_HE.xyz')
+            mol_old = pb.readfile("xyz","water_HE.xyz").next()
+        
+        elif j > 0:
+            w = open('water_HE_temp.xyz')
+            mol_old = pb.readfile("xyz","water_HE_temp.xyz").next()
+            
+        
+        
         lines_after_2 = w.readlines()[2:]
         # defines forcefield MMFF94
         forcefield = ob.OBForceField.FindForceField("MMFF94")
@@ -187,13 +201,14 @@ def hastings_all(n_gen,T,R,c):
         #print np.exp(- np.absolute(dE)/1.0*R*T), randomtal
         
         if dE <= 0:
-            os.system("mv water_temp.xyz water_HE.xyz")
+            os.system("mv water_temp.xyz water_HE_temp.xyz")
+
             #print "rotation:", energy_new
             #print "Accepted", j
         elif np.exp(- np.absolute(dE)/1.0*R*T) >= randomtal: #np.random.random():  downscale energy difference
         #elif np.random.choice(2, 1, p=[1-float(np.exp(-dE/R*T)), float(np.exp(-dE/R*T)])) == 1:
             #x, y, z = m.perturb(x, y, z, atom, no_atom)
-            os.system("mv water_temp.xyz water_HE.xyz")
+            os.system("mv water_temp.xyz water_HE_temp.xyz")
             #print "Hastings"
             c += 1
         #else:
@@ -210,65 +225,51 @@ def hastings_all(n_gen,T,R,c):
 
 
 
-def hastings_hydrogen(n_gen,T,R,c):
-    energy_list = [0 for n_gen in range(n_gen)] #new aproach
+def hastings_hydrogen(n_gen,T,R,c,energy_list):
+    percent = -10 #percentage counter
+    
+    os.system("cp water_HE.xyz water_HE_temp.xyz")
+    
     for j in range(n_gen):
-        for i in range(0,18,3):
-            #read file 
-            mol_old = pb.readfile("xyz","water_HE.xyz").next()
-            w = open('water_HE.xyz')
-            lines_after_2 = w.readlines()[2:]
-            # defines forcefield MMFF94
-            forcefield = ob.OBForceField.FindForceField("MMFF94")
-            no_atom = np.random.randint(18)
-            energy_old = get_energy(mol_old)
+        #read file 
         
-            atom = []
-            x = []
-            y = []
-            z = []
+        if j == 0:
+            w = open('water_HE.xyz')
+            mol_old = pb.readfile("xyz","water_HE.xyz").next()
+        
+        elif j > 0:
+            w = open('water_HE_temp.xyz')
+            mol_old = pb.readfile("xyz","water_HE_temp.xyz").next()
+    
+        #for i in range(0,18,3):
+            
+        lines_after_2 = w.readlines()[2:]
+        # defines forcefield MMFF94
+        forcefield = ob.OBForceField.FindForceField("MMFF94")
+        no_atom = np.random.randint(18)
+        while no_atom % 3 == 0:
+            no_atom = np.random.randint(18)
+            
+        energy_old = get_energy(mol_old)
+        
+        atom = []
+        x = []
+        y = []
+        z = []
 
-            for line in lines_after_2:
-                line = line.split()
-                atom.append(line[0])
-                x.append(float(line[1]))
-                y.append(float(line[2]))
-                z.append(float(line[3]))
+        for line in lines_after_2:
+            line = line.split()
+            atom.append(line[0])
+            x.append(float(line[1]))
+            y.append(float(line[2]))
+            z.append(float(line[3]))
             
             #f.write flyttet her fra
             
-            theta = np.random.uniform(0.0,np.pi*2.0)
-            theta2 = np.random.uniform(0.0,np.pi*2.0)
-            point = [x[1+i],y[1+i],z[1+i]] # 1st hydrogen to be rotated, first no 1
-            p1 = [x[0+i],y[0+i],z[0+i]] # oxygen, first no. 0
-            p2 = [x[2+i],y[2+i],z[2+i]] # other hydrogen no 2
-            new_point_x,new_point_y,new_point_z = Rot(point,p1,p2,theta)
-            x[1+i]=new_point_x
-            y[1+i]=new_point_y
-            z[1+i]=new_point_z
-                
-            point = [x[2+i],y[2+i],z[2+i]] #2nd hydrogen to be rotated
-            p2 = [x[1+i],y[1+i],z[1+i]]
-            new_point_x,new_point_y,new_point_z = Rot(point,p1,p2,theta2)
-            x[2+i]=new_point_x
-            y[2+i]=new_point_y
-            z[2+i]=new_point_z
-            
-            #write new coordinates to new temporary file
-            xyz = open('water_temp.xyz','w')
-
-            noatoms = str(len(x))
-            xyz.write('18\ntest\n')
-            for n in range(len(x)):
-                xyz.write('%s         %8s        %8s       %8s\n' % (atom[n],x[n],y[n],z[n]))
-            xyz.close()
-        
-        #f.write(str(j) + ',' + str(energy_old))
-        #f.write('\n')
-        
+        x,y,z = perturb(x,y,z,atom,no_atom)
+       
         #New energy write approach
         energy_list[j] += energy_old
-        
         
         mol_new = pb.readfile("xyz","water_temp.xyz").next()
         
@@ -278,22 +279,132 @@ def hastings_hydrogen(n_gen,T,R,c):
         
         #overwrite first file if the energy is lower
         dE = energy_new - energy_old
-        #print energy_new, energy_old, dE
+
         randomtal = np.random.random()
-        
-        print np.exp(- np.absolute(dE)/1.0*R*T), randomtal
+
         if dE <= 0:
-            os.system("mv water_temp.xyz water_HE.xyz")
-            #print "rotation:", energy_new
-            print "Accepted", j
-        elif np.exp(- np.absolute(dE)/1.0*R*T) >= randomtal: #np.random.random():  downscale energy difference
-        #elif np.random.choice(2, 1, p=[1-float(np.exp(-dE/R*T)), float(np.exp(-dE/R*T)])) == 1:
-            #x, y, z = m.perturb(x, y, z, atom, no_atom)
-            os.system("mv water_temp.xyz water_HE.xyz")
-            print "Hastings"
-            c += 1
-        else:
-            print "nothing changed" 
+            os.system("mv water_temp.xyz water_HE_temp.xyz")
+
+        elif np.exp(- np.absolute(dE)/1.0*R*T) >= randomtal: 
+            os.system("mv water_temp.xyz water_HE_temp.xyz")
+ 
+    
+        if j % (n_gen/10) == 0:
+            percent += 10
+            print percent
+                
+    return energy_list
+
+#DONE
+def rotation_hydrogen(n_gen,T,R,c,energy_list):
+    percent = -10 #percentage counter
+    os.system("cp water_HE.xyz water_HE_temp.xyz")
+    for j in range(n_gen):   
+        #read file 
         
+        if j == 0:
+            w = open('water_HE.xyz')
+            mol_old = pb.readfile("xyz","water_HE.xyz").next()
+        
+        elif j > 0:
+            w = open('water_HE_temp.xyz')
+            mol_old = pb.readfile("xyz","water_HE_temp.xyz").next()
+        
+        no_atom = np.random.randint(18)
+        while no_atom % 3 == 0:
+            no_atom = np.random.randint(18)
+
+        #read file 
+        lines_after_2 = w.readlines()[2:]
+        # defines forcefield MMFF94
+        forcefield = ob.OBForceField.FindForceField("MMFF94")
+        energy_old = m.get_energy(mol_old)
+        
+        energy_list[j] += energy_old
+        
+        atom = []
+        x = []
+        y = []
+        z = []
+
+        for line in lines_after_2:
+            line = line.split()
+            atom.append(line[0])
+            x.append(float(line[1]))
+            y.append(float(line[2]))
+            z.append(float(line[3]))
+            
+        x,y,z = perturb(x,y,z,atom,no_atom)
+        #write new coordinates to new temporary file
+        xyz = open('water_temp.xyz','w')
+
+        mol_new = pb.readfile("xyz","water_temp.xyz").next()
+        #calculate MMFF94 force field energies
+        energy_old = m.get_energy(mol_old)
+        energy_new = m.get_energy(mol_new)
+        
+        #overwrite first file if the energy is lower
+        if energy_new < energy_old:
+            os.system("mv water_temp.xyz water_HE_temp.xyz")
+            print j, "file overwritten:", energy_old, energy_new
+        
+        if j % (n_gen/10) == 0:
+            percent += 10
+            print percent
+
+    return energy_list
+
+
+def rotation_all(n_gen,T,R,c,energy_list):
+    percent = -10
+    
+    for j in range(n_gen):
+        #read file 
+        if j == 0:
+            w = open('water_HE.xyz')
+            mol_old = pb.readfile("xyz","water_HE.xyz").next()
+        
+        elif j > 0:
+            w = open('water_HE_temp.xyz')
+            mol_old = pb.readfile("xyz","water_HE_temp.xyz").next()
+            
+        lines_after_2 = w.readlines()[2:]
+        
+        no_atom = np.random.randint(18)
+        energy_old = get_energy(mol_old)
+        
+        energy_list[j] += energy_old
+        
+        atom = []
+        x = []
+        y = []
+        z = []
+
+        for line in lines_after_2:
+            line = line.split()
+            atom.append(line[0])
+            x.append(float(line[1]))
+            y.append(float(line[2]))
+            z.append(float(line[3]))
+        
+        
+        
+        theta = np.random.uniform(0.0,np.pi*2.0, 6)
+        
+        x, y, z= perturb(x,y,z, atom, no_atom) #perturbation in first H2O
+         
+        
+        mol_new = pb.readfile("xyz","water_temp.xyz").next()
+        #calculate MMFF94 force field energies
+        energy_old = get_energy(mol_old)
+        energy_new = get_energy(mol_new)
+        
+            #overwrite first file if the energy is lower
+        if energy_new < energy_old:
+            os.system("mv water_temp.xyz water_HE_temp.xyz")
+        
+        if j % (n_gen/10) == 0:
+            percent += 10
+            print percent
     return energy_list
 
--- run_simulation.py
+++ run_simulation.py
@@ -3,33 +3,39 @@ import molecule as m
 import openbabel as ob
 import numpy as np
 import os, sys
-import random_konf
 import molecule as m
 
 
-b = open("energy_hastings_all.csv", 'w') #match with name of simulation
+b = open("energy_all.csv", 'w') #match with name of simulation
 T = 300.0
 R = 0.00198588 #kcal/mol
 c = 0
 n_gen = 2000
 simulations=100
+energy_list = [0 for j in range(n_gen)]
 
 #This function has to be here. Can't get it to run from molecule.
 def save_data(n_gen, energy_list,simulations):
     #saves the average of the simulation energies
     for j in range(n_gen):
-        b.write(str(j) + ',' + str(energy_list[j]/simulations))
+        b.write(str(j) + ',' + str(energy_list[j]/simulations) + ',' + str(energy_list[j]))
         b.write('\n')
 
 
 #RUN SIMULATION
+
+#energy_new = m.random_konf() #deak
+#print energy_new
+
 for i in range(simulations):
     print "Simulation", i+1, "of", simulations
-    m.random_konf()
-    
+    print "Starting energy:", energy_list[0]
     #Choose one function to run:
-    energy_list = m.hastings_all(n_gen,T,R,c,)
-    #energy_list = m.hastings_hydrogen(n_gen,T,R,c,)
+    #energy_list = m.hastings_all(n_gen,T,R,c,energy_list)
+    #energy_list = m.hastings_hydrogen(n_gen,T,R,c,energy_list)
+    energy_list = m.rotation_all(n_gen,T,R,c,energy_list)
+    #energy_list = m.rotation_hydrogen(n_gen,T,R,c,energy_list)
     
-
+    #print energy_list[0]
+    
 save_data(n_gen, energy_list, simulations)
